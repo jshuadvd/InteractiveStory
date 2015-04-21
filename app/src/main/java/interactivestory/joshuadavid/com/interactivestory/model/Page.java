e741cd344c03de7611423883bcbc91b307ca6b90
JoshuaDavid <jshuadvd@icloud.com>
2015-04-21 12:35:19 -0700

    Added Getters and Setters for Choice options

--- app/src/main/java/interactivestory/joshuadavid/com/interactivestory/model/Page.java
+++ app/src/main/java/interactivestory/joshuadavid/com/interactivestory/model/Page.java
@@ -8,4 +8,42 @@ public class Page {
     private int mImageId;
     private String mText;
 
+    private Choice mChoice1;
+    private Choice mChoice2;
+
+
+
+    // Getters & Setters
+
+    public String getmText() {
+        return mText;
+    }
+
+    public void setmText(String mText) {
+        this.mText = mText;
+    }
+
+    public Choice getmChoice1() {
+        return mChoice1;
+    }
+
+    public void setmChoice1(Choice mChoice1) {
+        this.mChoice1 = mChoice1;
+    }
+
+    public Choice getmChoice2() {
+        return mChoice2;
+    }
+
+    public void setmChoice2(Choice mChoice2) {
+        this.mChoice2 = mChoice2;
+    }
+
+    public int getImageId() {
+        return mImageId;
+    }
+
+    public void setImageId(int id) {
+        mImageId = id;
+    }
 }
